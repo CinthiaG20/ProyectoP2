48ee3f5a2275c57526eed52c11e947df74a471d0
CinthiaG20 <cg9150153@gmail.com>
2024-09-23 17:27:17 -0300

    a

---
+++ Library/Personaje.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+
+namespace Library;
+
+public class Personaje
+{
+    private int vida;
+    public int Vida { get; set; }
+    private string tipo;
+    public string Tipo { get; set; }
+    private string nombre;
+    public string Nombre { get; set; }
+    private ArrayList listaAtk;
+    public ArrayList ListaAtk { get; set; }
+
+    public void Atacar(Personaje a)
+    {
+        
+    }
+
+    public void Defender(Personaje a)
+    {
+        
+    }
+}
